9c5f79d3d84a0282f5b07df2ac59a7bbc5bdab80
KimKyoungMin <1588760@daum.net>
2017-11-26 06:05:18 +0900

    main

--- src/movement.js
+++ src/movement.js
@@ -1,22 +1,13 @@
 
-  //Left arrow key `press` method
   left.press = function() {
-    //Change the ship's velocity when the key is pressed
     ship.vx = -5;
     ship.vy = 0;
   };
-
-  //Left arrow key `release` method
   left.release = function() {
-    //If the left arrow has been released, and the right arrow isn't down,
-    //and the ship isn't moving vertically:
-    //Stop the ship
     if (!right.isDown && ship.vy === 0) {
       ship.vx = 0;
     }
   };
-
-  //Up
   up.press = function() {
     ship.vy = -5;
     ship.vx = 0;
@@ -26,8 +17,7 @@
       ship.vy = 0;
     }
   };
-  //Right
-  right.press = function() {
+    right.press = function() {
     ship.vx = 5;
     ship.vy = 0;
   };
@@ -36,7 +26,6 @@
       ship.vx = 0;
     }
   };
-  //Down
   down.press = function() {
     ship.vy = 5;
     ship.vx = 0;
@@ -46,8 +35,3 @@
       ship.vy = 0;
     }
   };
-  //Set the game state
-  state = play;
-  //Start the game loop
-  gameLoop();
-}
