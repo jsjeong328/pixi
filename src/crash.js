f8804dff939d2c2015a68a1073ba5a9fe608d5ef
KimKyoungMin <1588760@daum.net>
2017-12-09 21:25:20 +0900

    충돌

--- src/crash.js
+++ src/crash.js
@@ -1,6 +1,6 @@
 function air_crash(a){
   var img_airp=document.getElementById("img_airp");
-  img_airp.src="img/ship.png";
+  img_airp.src="img/bomb.png";
   clearTimeout(opst_movex_time);
   clearTimeout(opst_movex_time1);
   clearTimeout(opst_movex_time2);
@@ -8,21 +8,19 @@ function air_crash(a){
   clearTimeout(opst_movex_time4);
   clearTimeout(opst_movex_time5);
   clearTimeout(game_point_init);
-
   our_airp=our_airp-1;
-
   game_count=0;
   clearInterval(movex_time);
   clearInterval(movey_time);
   x_on=1;
   y_on=1;
   if(our_airp==0){
-    over_game();
+   div3.innerHTML="Your Score is " + game_point + "!!";
+   over_game();
   }
   else {
     setTimeout(start_game,3000);
   }
-
 }
 function our_airp_inve(){
   if(our_airp==1){
