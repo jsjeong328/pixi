6910ba7734cdfa86451f090229ac35a1e56a6a1c
jsjeong328 <jsjeong328@gmail.com>
2017-12-18 23:49:55 +0900

    점수 업데이트 완성

--- api/score/update.php
+++ api/score/update.php
@@ -2,14 +2,12 @@
   session_start();
   require '../../system-critical/ConnectMainDB.php';
 
-
   $uid = $_SESSION['uid'];
-  $score = 11111;
+  $score = (int)$_POST['score'];
 
   $stmt = $connect->prepare("insert into Game (uID, Playtime, Score) VALUES (:uid, now(), :score)");
   $stmt->bindParam(':uid', $uid, PDO::PARAM_STR);
-  $stmt->bindValue(':score', $score, PDO::PARAM_INT);
+  $stmt->bindParam(':score', $score, PDO::PARAM_INT);
 
   $stmt->execute();
-
  ?>
